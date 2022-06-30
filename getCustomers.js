b73ce7fd6ed506aa3b052800989221821e4dfc1e
Hoi Yuen <hoikityuen@gmail.com>
2022-06-29 22:53:48 -0400

    add missing files to track

---
+++ getCustomers.js
@@ -0,0 +1,29 @@
+'use strict'
+const AWS = require('aws-sdk')
+
+module.exports.getCustomers = async (event) => {
+  const dynamoDb = new AWS.DynamoDB.DocumentClient()
+  const scanParams = {
+    TableName: process.env.DYNAMODB_CUSTOMER_TABLE
+  }
+  const result = await dynamoDb.scan(scanParams).promise()
+
+  if (result.Count === 0) {
+    return {
+      statusCode: 404
+    }
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      total: result.Count,
+      items: await result.Items.map(customer => {
+        return {
+          name: customer.primary_key,
+          email: customer.email
+        }
+      }) 
+    })
+  }
+}
