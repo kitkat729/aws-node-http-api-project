8aba26679ef5f30e3347183ca9b0e02334ee1b39
Hoi Yuen <hoikityuen@gmail.com>
2022-07-06 15:44:34 -0400

    RD-1 fix event body string format

--- createCustomer.js
+++ createCustomer.js
@@ -2,7 +2,7 @@
 const AWS = require('aws-sdk')
 
 module.exports.createCustomer = async (event) => {
-  const body = JSON.parse(Buffer.from(event.body, 'base64').toString())
+  const body = JSON.parse(event.body)
   const dynamoDb = new AWS.DynamoDB.DocumentClient()
   const putParams = {
     TableName: process.env.DYNAMODB_CUSTOMER_TABLE,
