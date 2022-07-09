cce0a793339cc8f027a7db30037561c49ed1eca2
Hoi Yuen <hoikityuen@gmail.com>
2022-07-08 20:31:49 -0400

    RD-3 improved error handling

--- functions/customers/createCustomer.ts
+++ functions/customers/createCustomer.ts
@@ -1,4 +1,4 @@
-import { DynamoDB } from 'aws-sdk'
+import { DynamoDB, AWSError } from 'aws-sdk'
 import { APIGatewayProxyHandlerV2, APIGatewayProxyResultV2 } from 'aws-lambda'
 
 const dynamoDb = new DynamoDB.DocumentClient()
@@ -13,26 +13,58 @@ const dynamoDb = new DynamoDB.DocumentClient()
 const handler: APIGatewayProxyHandlerV2 = async (event, context): Promise<APIGatewayProxyResultV2> => {
   if (!event?.body) {
     return {
-      "statusCode": 400
+      "statusCode": 400,
+      "body": JSON.stringify({
+        "code": "MalformedInput",
+        "errorMessage": "Malformed input"
+      })
     }
   }
 
-  // AWS and local behave differently. AWS encodes body data in base64 format, but local does not encode.
-  // Here we check the isBase64Encode flag to decide whether body data needs unencoding
-  const body = JSON.parse(event.isBase64Encoded? Buffer.from(event.body, 'base64').toString() : event.body)
+  const customer = JSON.parse(Buffer.from(event.body, event?.isBase64Encoded ? 'base64' : 'utf8').toString())
+
+  if (!customer?.name) {
+    return {
+      "statusCode": 400,
+      "body": JSON.stringify({
+        "code": "MissingParameter",
+        "errorMessage": "`name` is required"
+      })
+    }
+  }
+
+  const {name: primary_key, ...rest} = customer;
   const putParams = {
     TableName: process.env.DYNAMODB_CUSTOMER_TABLE,
     Item: {
-      primary_key: body.name,
-      email: body.email
-    }
+      primary_key: primary_key,
+      ...rest
+    },
   } as DynamoDB.DocumentClient.PutItemInput
 
-  await dynamoDb.put(putParams).promise()
+  let response
 
-  return {
-    "statusCode": 201
-  }
+  // dynamoDb immediately throws an error for invalid input
+  // putItem will replace existing item with new item 
+  await dynamoDb.put(putParams).promise()
+    .then(result => {
+      response = {
+        "statusCode": 201,
+        "headers": {
+          "Location": "new-resource-location" 
+        }
+      }
+    })
+    .catch((err: AWSError) => {
+      response = {
+        "statusCode": 500,
+        "body": JSON.stringify({
+          "code": "InternalServerError"
+        })
+      }
+    })
+
+  return await response
 }
 
 export default handler
