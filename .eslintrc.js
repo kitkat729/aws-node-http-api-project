408fed0743318780e3b9701df2e4241e0aa1a0c3
Hoi Yuen <hoikityuen@gmail.com>
2022-07-15 11:43:25 -0400

    build: RD-13 Using include in tsconfig alone did fix the eslint parserOptions.project errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,7 +16,6 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: './tsconfig.eslint.json',
-    tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
   rules: {
